5ba4131c3fb02529ebe3b96ddcc902c5c680baa5
Kanasansoft <kanasansoft@gmail.com>
2012-01-20 00:57:02 +0900

    mavenのプラグインで作成したandroidプロジェクトを修正したものを追加。

---
+++ src/main/java/com/kanasansoft/android/WSCamera/WSCamera.java
@@ -0,0 +1,17 @@
+package com.kanasansoft.android.WSCamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WSCamera extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+	}
+
+}
